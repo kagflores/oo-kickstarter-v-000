26cc8c73d192bdbca5afdf94abed79b9574cbcc1
Karen Flores <karen.ag.flores@gmail.com>
2018-07-25 23:59:37 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,8 +11,8 @@ class Backer
   def back_project(project)
     @backed_projects << project
     new_backer = @name
-    binding.pry
-    #Project.add_backer << new_backer
+    #binding.pry
+    Project.add_backer << new_backer
     #self.backed_projects = Project.find_or_create(project)
     #self.find_by_project
   end
