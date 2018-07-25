6434d63ef13d1125d76f65cbe47037b834a58ac6
Karen Flores <karen.ag.flores@gmail.com>
2018-07-25 23:38:10 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,31 @@
+class Project
+  attr_accessor :backers, :title
+  @@all = []
+
+  def initialize(title)
+    @backers = []
+    @title = title
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    self.backers = Backer.find_or_create(backer)
+  end
+
+  def self.find_or_create(title)
+    if @@all.any?{|n| n.title = title}
+    else
+      new_title = self.new(title)
+      @@all << new_title
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+end
